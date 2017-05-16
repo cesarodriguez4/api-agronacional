f746f4af0b3e1e32995b60c9f5b395cecfb1b00b
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-05-16 16:25:20 -0400

    securelessapps

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,17 @@ const nodemailer = require('nodemailer');
 let transporter = nodemailer.createTransport({
 	service: 'gmail',
 	auth: {
-		user: 'cesarodriguez4@gmail.com', 
-		pass: 'Vcesar04!'
+		user: 'agronacional1@gmail.com', 
+		pass: 'agronacionaldelcentro'
 	}
+});
+
+transporter.verify( (error, success) => {
+   if (error) {
+        console.log(error);
+   } else {
+        console.log('Server is ready to take our messages');
+   }
 });
 
 /* GET home page. */
