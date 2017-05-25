b9845bb4fe4b0af802488156a0b9fdf0f075d75f
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-05-25 01:43:53 -0400

    que vale mas

--- routes/login.js
+++ routes/login.js
@@ -2,22 +2,22 @@ const sql = require('sql-crud');
 let crud = new sql('mysql');
 module.exports = (app, con) => {
 	app.post('/login', (req, res) => {
-		const nombre = req.body.nombre;
+		console.log('culooo');
+		const usuario = req.body.usuario;
 		const password = req.body.password;
+		console.log(usuario);
 		crud.select(con, {
 			select: '*',
 			from: 'USUARIOS',
-			where: {nombre, password}
+			where: {usuario, password}
 		}, (error, result) => {
 			if (error) {
 				res.send(error);
 			}
-			if(result.length > 0) {
+			if (result.length > 0 ) {
 				res.send({success: true});
 			}
-			else {
-				res.send({error: 'Password y usuario incorrectos'});
-			}
+			res.send({error: 'No existe el usuario'});
 		}, true);
 	});
 }
